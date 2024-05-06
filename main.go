48b5748f609d20da413144f1821aa86d4e142606
Jason Lunn <61010854+jasonleelunn@users.noreply.github.com>
2024-05-06 22:18:22 +0100

    feat(server): add and serve htmx bundle, setup test button

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,14 +8,16 @@ import (
 func main() {
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		component := hello("Jason")
+	router.HandleFunc("GET /foo", func(w http.ResponseWriter, r *http.Request) {
+		component := Page()
 		component.Render(r.Context(), w)
 	})
 
-	router.HandleFunc("GET /foo", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Got foo")
+	router.HandleFunc("POST /clicked-hi", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Clicked Me!"))
 	})
+
+	router.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
 	err := http.ListenAndServe(":4040", router)
 
