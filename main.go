b6eace4ea39567a06ed7c6507ed679b599cedf5f
Jason Lunn <61010854+jasonleelunn@users.noreply.github.com>
2024-05-07 23:03:29 +0100

    feat: add game grid, setup game state

--- main.go
+++ main.go
@@ -5,16 +5,51 @@ import (
 	"net/http"
 )
 
+type Token int
+
+type Game struct {
+	currentToken Token
+	grid         map[string]bool
+}
+
+const (
+	naught Token = iota
+	cross
+)
+
+var (
+	tokens = map[Token]string{
+		naught: "O",
+		cross:  "X",
+	}
+)
+
 func main() {
+	game := Game{currentToken: naught, grid: make(map[string]bool)}
+
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /foo", func(w http.ResponseWriter, r *http.Request) {
-		component := Page()
-		component.Render(r.Context(), w)
+	router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		Page().Render(r.Context(), w)
 	})
 
-	router.HandleFunc("POST /clicked-hi", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Clicked Me!"))
+	router.HandleFunc("POST /cells/{cell}", func(w http.ResponseWriter, r *http.Request) {
+		cell := r.PathValue("cell")
+
+		if game.grid[cell] == true {
+			w.WriteHeader(http.StatusTeapot)
+			return
+		}
+
+		game.grid[cell] = true
+
+		w.Write([]byte(tokens[game.currentToken]))
+
+		if game.currentToken == naught {
+			game.currentToken = cross
+		} else {
+			game.currentToken = naught
+		}
 	})
 
 	router.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
